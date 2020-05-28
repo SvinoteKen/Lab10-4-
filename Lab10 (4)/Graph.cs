62f4bd8dd2ad3ed9b67cf4fb6d9a273b0f03df7d
свин свин <stastab2013@gmail.com>
2020-05-28 19:49:36 +0300

    Add project files.

---
+++ Lab10 (4)/Graph.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab10__4_
+{
+    class Graph
+    {
+        private int V = 0;
+
+        private int[,] graph = null;
+
+        public Graph(int[,] matrix, int numOfVert)
+        {
+            graph = matrix;
+            V = numOfVert;
+        }
+
+        public Stack<int> backChain(int[] p, int startPos, int endPos)
+        {
+            int pos = endPos;
+
+            Stack<int> pathStack = new Stack<int>();
+            pathStack.Push(pos);
+
+            while (pos != startPos)
+            {
+                pos = p[pos];
+                pathStack.Push(pos);
+            }
+
+            return pathStack;
+        }
+
+        public Stack<int> BFS(int startPos, int endPos)
+        {
+            Queue<int> q = new Queue<int>();
+
+            int[] vPath = new int[V];
+
+            int[] checkedv = new int[V];
+            q.Enqueue(startPos);
+            checkedv[startPos] = 1;
+
+            while (q.Count > 0)
+            {
+                int i = q.Dequeue();
+
+                for (int j = 0; j < V; j++)
+                {
+                    if (graph[i, j] > -1 && checkedv[j] == 0)
+                    {
+                        checkedv[j] = 1;
+                        q.Enqueue(j);
+                        vPath[j] = i;
+                        if (j == endPos)
+                        {
+                           return backChain(vPath, startPos, endPos);
+                        }
+                        
+                    }
+                }
+            }
+            return null;
+        }
+       
+    }
+}
---
+++ Lab10 (4)/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab10__4_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] arr = 
+            {
+                //1  2  3  4  5  6  7  8
+                {-1,120,60,-1,-1,-1,-1,-1},  //1
+				{120,-1,-1,-1,-1,70,100,-1}, //2 
+				{60,-1,-1,30,100,120,-1,-1}, //3 
+				{-1,-1,30,-1,-1,-1,-1,-1},   //4
+				{-1,-1,100,-1,-1,-1,-1,50},  //5
+				{-1,70,120,-1,50,-1,60,20},  //6
+				{-1,100,-1,-1,-1,60,-1,-1},  //7
+				{-1,-1,-1,-1,50,20,-1,-1}    //8
+			};
+
+            Graph graph = new Graph(arr, 8);
+
+            Console.WriteLine("Введите номер города от 1-8\n");
+            int X = int.Parse(Console.ReadLine());
+            while (X == 0 || X > 8)
+            {
+                Console.WriteLine("Значение введено не верно.Введите верное значение от 1 до 8:");
+                X = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Маршруты:\n");
+            for (int j = 0; j < 8; j++)
+            {
+                if (X != j + 1)
+                {
+                    Stack<int> BFS = graph.BFS(X - 1, j);
+                    PrintPath(BFS,arr);
+                }
+            }
+
+        }
+        private static void PrintPath(Stack<int> path,int[,] arr)
+        {
+            if (path.Count == 0)
+            {
+                Console.WriteLine("Путь не найден");
+            }
+            else
+            {
+                int [] matrix =path.ToArray();
+                int sum = 0;
+                for (int i = 0; i < matrix.Length; i++)
+                {
+                    if (i + 1 == matrix.Length) { break; }
+                    sum += arr[matrix[i], matrix[i + 1]];
+                }
+                if (sum <= 200)
+                {
+                    Console.WriteLine(string.Join(" -> ", path.Select(x => (x + 1).ToString())) + "(" + sum + "км)");
+                }
+            }
+
+        }
+
+    }
+}
